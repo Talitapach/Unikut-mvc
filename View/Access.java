97c13913dbb53f65773ba748d94e46e432001bad
Beatriz Ramos <beatrizramosfig@hotmail.com>
2022-05-05 13:52:55 +0000

    Commit in class

---
+++ View/Access.java
@@ -0,0 +1,30 @@
+package View;
+
+public class Access {
+    
+    public void interfaceInicial(){
+        System.out.println("  _    _       _ _          _   \n" +
+        " | |  | |     (_) |        | |  \n" +
+        " | |  | |_ __  _| | ___   _| |_ \n" +
+        " | |  | | '_ \\| | |/ / | | | __|\n" +
+        " | |__| | | | | |   <| |_| | |_ \n" +
+        "  \\____/|_| |_|_|_|\\_\\\\__,_|\\__|");
+        System.out.println("");
+        System.out.println("Wellcome :)");
+        System.out.println("Please, choose a option");
+        System.out.println("1 - Login");
+        System.out.println("2 - Sign up");
+    }
+
+    public void interfaceLogin(){
+        System.out.println("");
+        System.out.println("1 - Edit your account");
+        System.out.println("2 - Add a new friend");
+        System.out.println("3 - Send message");
+        System.out.println("4 - View messages");
+        System.out.println("5 - Requests");
+        System.out.println("6 - Remove a friend");
+        System.out.println("7 - FriendList");
+        System.out.println("8 - Log out");
+    }
+}
--- View/Main.java
+++ View/Main.java
@@ -6,19 +6,10 @@ import Model.User;
 
 public class Main{
     public static void main(String[] args) {
+        Access interfaceUnikut = new Access();
         int option, option2;
         Scanner s = new Scanner(System.in);
-        System.out.println("  _    _       _ _          _   \n" +
-        " | |  | |     (_) |        | |  \n" +
-        " | |  | |_ __  _| | ___   _| |_ \n" +
-        " | |  | | '_ \\| | |/ / | | | __|\n" +
-        " | |__| | | | | |   <| |_| | |_ \n" +
-        "  \\____/|_| |_|_|_|\\_\\\\__,_|\\__|");
-        System.out.println("");
-        System.out.println("Wellcome :)");
-        System.out.println("Please, choose a option");
-        System.out.println("1 - Login");
-        System.out.println("2 - Sign up");
+        interfaceUnikut.interfaceInicial();
         option = s.nextInt();
         switch(option) {
             case 1:
@@ -30,7 +21,8 @@ public class Main{
                 System.out.println("Create a password");
                 String password = s.next();
                 System.out.println("Type your name");
-                String name = s.next();
+                String name = s.nextLine();
+                name = s.nextLine();
                 User newUser = new User(username, password, name);
               break;
             default:
